dab20c0279743ad5265066703f0acb57d1945497
PabloIzquierdo <pabloizquierdofernandez7@gmail.com>
2021-05-12 12:24:35 +0200

    Application Web Files

---
+++ FlusWeb/Controllers/ScaffoldController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace FlusWeb.Controllers
+{
+    public class ScaffoldController : Controller
+    {
+        //
+        //GET: /Scaffold/
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        //GET: /Scaffold/Welcome/
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID : {ID}");
+        }
+    }
+}
